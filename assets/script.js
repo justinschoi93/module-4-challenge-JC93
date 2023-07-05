570450510158bc726e5a1b38d294c0e6aff4c98e
justinschoi93 <justinschoi93@gmail.com>
2023-07-05 13:13:37 -0700

    index.html created

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+var timer = 60;
+
+//set interval so every second, the timer goes down by 1
+//when timer reaches 0, clear interval and save high score. 
+//define askQuestions function
+//when correct answer is selected, a point is rewarded. 
+//when an incorrect answer is selected, 10 seconds are deducted from the timer. 
+//when we are out of questions, clearInterval and save high score. 
